0fd92497676e1353ef666ed8f2ef487c8ab5fc1b
Gonzalo Toyos <gonzalotoyos2001@gmail.com>
2022-08-18 22:36:44 -0300

    Primer commit de prueba

---
+++ src/App.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Ingresa 3 números:");
+        Scanner consola = new Scanner(System.in);
+        int n1 = consola.nextInt();
+        int n2 = consola.nextInt();
+        int n3 = consola.nextInt();
+        consola.close();
+
+        System.out.println("La suma da: " + suma(n1, n2, n3));
+    }
+
+    public static int suma(int n1, int n2, int n3){
+        return n1 + n2 + n3;
+    }
+}
